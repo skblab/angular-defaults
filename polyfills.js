1d2aac00e4a4599d9665dbefe852a91298a43972
CvetkovSV <cvetkovsv@skblab.ru>
2019-06-10 03:39:02 +0500

    7.215.8

--- polyfills.js
+++ polyfills.js
@@ -18,35 +18,6 @@ exports.__esModule = true;
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-require("core-js/es6/symbol");
-require("core-js/es6/object");
-require("core-js/es6/function");
-require("core-js/es6/parse-int");
-require("core-js/es6/parse-float");
-require("core-js/es6/number");
-require("core-js/es6/math");
-require("core-js/es6/string");
-require("core-js/es6/date");
-require("core-js/es6/array");
-require("core-js/es6/regexp");
-require("core-js/es6/map");
-require("core-js/es6/set");
-/*** IE11 *********************/
-require("core-js/es7/array");
-require("core-js/es7/asap");
-require("core-js/es7/error");
-require("core-js/es7/map");
-require("core-js/es7/math");
-require("core-js/es7/object");
-require("core-js/es7/observable");
-require("core-js/es7/set");
-require("core-js/es7/string");
-require("core-js/es7/symbol");
-require("core-js/es7/system");
-/** Evergreen browsers require these. **/
-require("core-js/es6/reflect");
-require("core-js/es7/reflect");
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 /**
